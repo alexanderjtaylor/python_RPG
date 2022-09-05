93a50d44cc4965a41b3319c478c95a59bfcc4b5c
Alex <alexandertaylor315@gmail.com>
2022-09-04 23:01:23 -0400

    Built dictionaries for each character, lists of game characters, and user prompt to select attack type

---
+++ main.py
@@ -0,0 +1,31 @@
+
+Hercules = {
+    "Health" : 100,
+    "Attack Power" : 10,
+    "Attack Moves" : ["Punch", "Kick", "Jumping Punch", "Grapple"]
+}
+
+Nemean_Lion = {
+    "Health" : 100,
+    "Attack Power" : 5,
+    "Attack Moves" : ["Punch", "Kick", "Jumping Punch", "Grapple"]
+}
+
+Lernaean_Hydra = {
+    "Health" : 100,
+    "Attack Power" : 10,
+    "Attack Moves" : ["Punch", "Kick", "Jumping Punch", "Grapple"]
+}
+
+Cerberus = {
+    "Health" : 100,
+    "Attack Power" : 15,
+    "Attack Moves" : ["Punch", "Kick", "Jumping Punch", "Grapple"]
+}
+
+game_characters = [Hercules, Nemean_Lion, Lernaean_Hydra, Cerberus]
+
+def user_hercules():
+    user_input = input("Select an attack for Hercules: Punch, Kick, Jumping Punch, Grapple")
+
+user_hercules()
